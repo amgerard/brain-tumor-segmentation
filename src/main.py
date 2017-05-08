23723631c40e64cad616ba2a249b99689ae786e6
Alex Gerard <alex.gerard.3.14@gmail.com>
2017-05-08 04:16:15 -0500

    clean up

--- src/main.py
+++ src/main.py
@@ -25,32 +25,39 @@ from get_data2 import get_data, get_val_data, get_trn_data
 
 if __name__ == '__main__':
 	
-	name = 'x1'
+	name = 'p3'
 	logs_dir = 'logs/' + name + '/'
         cp_dir = 'chpts/' + name + '/'
         rid = name
 
-	X, Y = get_trn_data()
-	#testX, testY = get_val_data()
-	print('done loading',X.shape,Y.shape)
-	#print('done loading',testX.shape,testY.shape)
-	print(np.sum(Y, axis=0))
-	#print(np.sum(testY, axis=0))
+	load_trn,load_val = True,False
+	if load_trn:
+		X, Y = get_trn_data()
+		print('done loading',X.shape,Y.shape)
+		print(np.sum(Y, axis=0))
+	if load_val:
+		testX, testY = get_val_data()
+		print('done loading',testX.shape,testY.shape)
+		print(np.sum(testY, axis=0))
 	
 	with tf.device('/gpu:0'):
-		network = cnn_3x3.get_network()
-		#network = cnn_2path.get_network()
+		#network = cnn_3x3.get_network()
+		network = cnn_2path.get_network()
 		#network = cnn_naive.get_network()
 	
         model = tflearn.DNN(network, tensorboard_verbose=2, tensorboard_dir=logs_dir, checkpoint_path=cp_dir)
+       	#model.load('n3.tfl')
        	#model.load(name + '.tfl')
 	#evaluate.evaluate_model(model,testX,testY)
 
         # train
         print('start training')
-        model.fit({'input': X}, {'target': Y}, n_epoch=2, # 20
-                   #validation_set=({'input': testX}, {'target': testY}),
-                   snapshot_step=100, show_metric=True, run_id=rid)
+        try:
+		model.fit({'input': X}, {'target': Y}, n_epoch=2, # 20
+			   #validation_set=({'input': testX}, {'target': testY}),
+			   snapshot_step=100, show_metric=True, run_id=rid)
+		model.save(name + ".tfl")
+	except KeyboardInterrupt:
+		# Manually save model
+		model.save(name + ".tfl")
 
-        # Manually save model
-        model.save(name + ".tfl")
